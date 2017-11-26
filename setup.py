c1f9c512a6b649028a80771abb05a2eda47d1a0c
Ľubomír Kučera <lubomir.kucera@devinsolutions.com>
2017-11-26 14:10:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,28 @@
 
 from setuptools import find_packages, setup
 
+with open('README.rst') as readme:
+    long_description = readme.read()
+
 setup(
     name='cdist-manifestation',
-    version='1.0.0',
+    version='0.1.0',
     description='A helper for writing cdist manifests in Python',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+    ],
     keywords=[
         'cdist'
     ],
     author='Devin Solutions s.r.o.',
     author_email='info@devinsolutions.com',
+    url='https://github.com/devinsolutions/python-cdist-manifestation',
     license='MIT',
     packages=find_packages(),
     setup_requires=[
